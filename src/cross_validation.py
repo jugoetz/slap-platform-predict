bef76434ba628055157d6eb990039759e3179029
jugoetz <70372980+jugoetz@users.noreply.github.com>
2022-05-10 17:34:33 +0200

    add ShuffleSplit for single fold

--- src/cross_validation.py
+++ src/cross_validation.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 import torch
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, ShuffleSplit
 from torch.utils.data import DataLoader
 
 from src.data.dataloader import collate_fn
@@ -9,21 +9,23 @@ from src.train import train
 from src.util.logging import generate_run_id
 
 
-def cross_validate(hparams, data, n_folds=5, strategy="KFold"):
+def cross_validate(hparams, data, n_folds=5, strategy="KFold", save_models=False):
     """
-    Train a model under cross-validation.
+    Trains a model under cross-validation. Returns the validation metrics' mean and std.
 
     Args:
         hparams (dict): Model configuration. See model for details.
         data (torch.utils.data.Dataset): Data set to run CV on.
-        n_folds (int): Number of folds.
+        n_folds (int): Number of folds. If 1, a 4:1 ShuffleSplit will be used regardless of strategy.
         strategy (str): CV strategy. Supported: {"KFold"}. Defaults to "KFold".
 
     Returns:
         dict: Metrics returned by train(), aggregated across folds (mean and standard deviation).
     """
     # set up splitter
-    if strategy == "KFold":
+    if n_folds == 1:
+        splitter = ShuffleSplit(test_size=0.2, random_state=42)
+    elif strategy == "KFold":
         splitter = KFold(n_splits=n_folds, shuffle=True, random_state=42)
     else:
         raise ValueError(f"Invalid strategy {strategy}")
@@ -34,11 +36,13 @@ def cross_validate(hparams, data, n_folds=5, strategy="KFold"):
     # iterate folds
     metrics = defaultdict(list)
     for i, (train_idx, val_idx) in enumerate(splitter.split(list(range(len(data))))):
+        if i >= n_folds:
+            break
         data_train = [data[i] for i in train_idx]
-        data_val = [data[i] for i in train_idx]
+        data_val = [data[i] for i in val_idx]
         train_dl = DataLoader(data_train, batch_size=32, shuffle=True, collate_fn=collate_fn)
         val_dl = DataLoader(data_val, batch_size=32, collate_fn=collate_fn)
-        fold_metrics = train(train_dl, val_dl, hparams, run_id=f"{cv_run_id}_fold{i}")
+        fold_metrics = train(train_dl, val_dl, hparams, run_id=f"{cv_run_id}_fold{i}", save_model=save_models)
         for k, v in fold_metrics.items():
             metrics[k].append(v)
 

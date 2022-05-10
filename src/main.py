658842f3fc6765401c6f4e8eedd780072b0af610
jugoetz <70372980+jugoetz@users.noreply.github.com>
2022-05-10 09:15:14 +0200

    minor changes

--- src/main.py
+++ src/main.py
@@ -5,40 +5,49 @@ import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
 import wandb
 
-from src.data.dataloader import SLAPDataModule
+from src.data.dataloader import SLAPDataModule, SLAPDataset
 from src.model.mpnn_classifier import DMPNNModel
 from src.util.configuration import CONFIG
 from src.util.definitions import DATA_ROOT, PROJECT_DIR, LOG_DIR_ABS
-
+from src.cross_validation import cross_validate
+from src.hyperopt import optimize_hyperparameters
 # logging
 os.environ["WANDB_DIR"] = LOG_DIR_ABS
 os.environ["WANDB_MODE"] = "online"
 wandb_group_name = wandb.util.generate_id()
 wandb_project = "slap-gnn"
-cross_validate = 5
+# cross_validate = 5
 log_cv_mean = True
 
 
 # TODO: have the possibility to pass train and test set, then internally split train into train and val and do cv on it
 
 # data
-dm = SLAPDataModule(data_dir=DATA_ROOT,
-                    split_dir=DATA_ROOT / CONFIG["split_dir_name"],
-                    cross_validate=cross_validate,
-                    reaction=CONFIG["reaction"],
-                    smiles_columns=("SMILES", ),
-                    label_column="targets",
-                    batch_size=32,
-                    )
-dm.prepare_data()
-dm.setup()
+# dm = SLAPDataModule(data_dir=DATA_ROOT,
+#                     split_dir=DATA_ROOT / CONFIG["split_dir_name"],
+#                     cross_validate=cross_validate,
+#                     reaction=CONFIG["reaction"],
+#                     smiles_columns=("SMILES", ),
+#                     label_column="targets",
+#                     batch_size=32,
+#                     )
+# dm.prepare_data()
+# dm.setup()
+
+data = SLAPDataset(raw_dir=DATA_ROOT, reaction=True, smiles_columns=("SMILES", ), label_column="targets")
+CONFIG["atom_feature_size"] = data.atom_feature_size
+CONFIG["bond_feature_size"] = data.bond_feature_size
+print(optimize_hyperparameters(CONFIG, data))
+exit(0)
+print(cross_validate(CONFIG, data, ))
+exit(0)
 
 # run training
 if cross_validate:
     metrics = []
     for fold in range(cross_validate):
         wandb.init(group=wandb_group_name, project=wandb_project, name=f"fold{fold}")
-        model = DMPNNModel(**CONFIG, atom_feature_size=dm.atom_feature_size, bond_feature_size=dm.bond_feature_size)
+        model = DMPNNModel(**CONFIG)
         # trainer
         logger = WandbLogger()
         trainer = pl.Trainer(max_epochs=CONFIG["training"]["max_epochs"], log_every_n_steps=1,
@@ -59,7 +68,7 @@ if cross_validate:
         wandb.finish()
 else:
     wandb.init(group=wandb_group_name, project=wandb_project)
-    model = DMPNNModel(**CONFIG, atom_feature_size=dm.atom_feature_size, bond_feature_size=dm.bond_feature_size)
+    model = DMPNNModel(**CONFIG)
     # trainer
     logger = WandbLogger()
     trainer = pl.Trainer(max_epochs=CONFIG["training"]["max_epochs"], log_every_n_steps=1, default_root_dir=PROJECT_DIR,

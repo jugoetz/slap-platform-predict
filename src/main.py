ba5c99460a4e8b65e8074d2e04566f9b3ea83604
jugoetz <70372980+jugoetz@users.noreply.github.com>
2022-05-11 18:15:28 +0200

    add train-test split

--- src/main.py
+++ src/main.py
@@ -3,6 +3,10 @@ from src.util.configuration import CONFIG
 from src.util.definitions import DATA_ROOT
 from src.cross_validation import cross_validate
 
+import torch
+import pandas as pd
+
+run_test_set = False
 
 # load data
 data = SLAPDataset(name=CONFIG["data_name"], raw_dir=DATA_ROOT, reaction=CONFIG["reaction"], smiles_columns=("SMILES", ), label_column="targets")
@@ -12,11 +16,17 @@ CONFIG["atom_feature_size"] = data.atom_feature_size
 CONFIG["bond_feature_size"] = data.bond_feature_size
 
 # TODO split data
-
-
+train_idx = pd.read_csv(DATA_ROOT / CONFIG["split_dir_name"] / "fold_0_train_idx.csv", header=0).values.flatten()
+test_idx = pd.read_csv(DATA_ROOT / CONFIG["split_dir_name"] / "fold_0_test_idx.csv", header=0).values.flatten()
+train = torch.utils.data.Subset(data, train_idx)
+test = torch.utils.data.Subset(data, test_idx)
 
 # run training
-aggregate_metrics, fold_metrics = cross_validate(CONFIG, data, 1, save_models=True, return_fold_metrics=True)
+aggregate_metrics, fold_metrics = cross_validate(CONFIG, train, 1, save_models=True, return_fold_metrics=True)
+
+if run_test_set:
+    # TODO evaluate (need to return the trained model from cv)
+    ...
 
 
 

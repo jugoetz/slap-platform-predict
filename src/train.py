8b799ac63324b45e450e001ee2d5eda1142e2448
jugoetz <70372980+jugoetz@users.noreply.github.com>
2022-05-11 16:05:22 +0200

    add logger

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
 
 from src.model.mpnn_classifier import DMPNNModel
 from src.util.definitions import PROJECT_DIR, LOG_DIR
@@ -15,7 +16,7 @@ def train(train, val, hparams, run_id=None, test=None, return_test_metrics=False
         val: (torch.utils.data.DataLoader): Validation data
         test: (torch.utils.data.DataLoader, optional): Test data. Only used if return_test_metrics is True.
         hparams (dict): Model hyperparameters
-        run_id (optional, str): Unique id to identify the run. If None, will generate an ID containing the current datetime and a random integer.
+        run_id (optional, str): Unique id to identify the run. If None, will generate an ID containing the current datetime.
         return_test_metrics (bool): Whether to return metrics on test set. If True, test has to be given.
             Defaults to False.
         save_model (bool): Whether to save the trained model weights to disk. Defaults to False.
@@ -40,7 +41,7 @@ def train(train, val, hparams, run_id=None, test=None, return_test_metrics=False
     else:
         accelerator = "cpu"
     trainer = pl.Trainer(max_epochs=hparams["training"]["max_epochs"], log_every_n_steps=1,
-                         default_root_dir=PROJECT_DIR, logger=False, accelerator=accelerator)
+                         default_root_dir=PROJECT_DIR, logger=WandbLogger(), accelerator=accelerator)
     # run training
     trainer.fit(model, train_dataloaders=train, val_dataloaders=val)
 
